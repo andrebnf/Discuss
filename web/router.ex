68537ec7c9852c97991e8b54a93ac9a7ce24944b
Andre Bonfatti <andre.bonfatti@movile.com>
2016-12-02 00:47:01 -0200

    adding form/create topic

--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,8 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/topic/new", TopicController, :new
+    get "/topics/new", TopicController, :new
+    post "/topics", TopicController, :create
   end
 
 end
